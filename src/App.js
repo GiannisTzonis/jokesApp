8c5061296b2037e6b2d971676a3bdd24e0563e34
GiannisTzonis <tzonis.john@hotmail.com>
2022-11-25 13:36:49 +0200

    cleared

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Card from "./components/Card"
-import data from "./data"
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+// import data from "./data"
 
 export default function App() {
-    const cards = data.map(item => {
-        return (
-            <Card
-                key={item.id}
-                item={item}
-            />
-        )
-    })        
-    
-            // <Hero />
-    return (
-        <div>
-            <Navbar />
-            <section className="cards-list">
-                {cards}
-            </section>
-        </div>
-    )
+  //   const cards = data.map((item) => {
+  //     return <Card key={item.id} item={item} />;
+  //   });
+
+  // <Hero />
+  return (
+    <div>
+      <Navbar />
+      <section className="cards-list">{cards}</section>
+    </div>
+  );
 }
